0aa813edcc119d451cb7e423a35531de9702f369
Diego Rodrigues <diegormb@live.com>
2020-07-24 16:33:25 -0300

    feat: create SignUpController with handle method

--- src/controller.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('', () => {
-  test('testando o jest', () => {
-    expect(1).toBe(1)
-  })
-})
---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signUp'
+
+describe('SignUp controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any@mail.com',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
