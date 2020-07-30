bbbf4e8d5e6c004aef12ca1285c5c3274b4e21c7
Diego Rodrigues <diegormb@live.com>
2020-07-30 18:11:59 -0300

    refactor: move protocols imports to a single file

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signUp'
 import { MissingParramError, InvalidParramError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols/email-validator'
+import { EmailValidator } from '../protocols'
 
 interface SutTypes {
   sut: SignUpController
